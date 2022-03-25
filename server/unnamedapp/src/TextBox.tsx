578d4572485e2b14c0a443423826cbf0db5b54f8
ansoson <84486319+ansoson@users.noreply.github.com>
2022-03-25 19:12:47 -0400

    completed :D

---
+++ server/unnamedapp/src/TextBox.tsx
@@ -0,0 +1,9 @@
+function TextBox(props: any) {
+    return (
+        <div className = "TextBox">
+            <label>{props.label}</label>
+            <input type={'text'} onChange={event => props.change(event.target.value)}/>
+        </div>
+    );
+}
+export default TextBox
